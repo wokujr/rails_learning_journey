93ad52848bd16e46e8c10f0f5ca8b9ffbe7f78f1
wokujr <wokujr@gmail.com>
2023-07-27 08:43:11 +0700

    Created sign in and logout functionality also fix few bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # this mean before we run everything check if Current.user is set. reference shared/_navbar_html.erb
+  before_action :set_current_user
+
+  def set_current_user
+    #check if user exist and logged in
+    if session[:user_id]
+      #find_by will act same like logged out if user/data deleted from database
+      Current.user = User.find_by(id: session[:user_id])
+    end
+  end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,4 @@
 class MainController < AboutController
     def index
-        #check if user exist and logged in
-        if session[:user_id]
-            
-            #find_by will act same like logged out if user/data deleted from database
-            @user = User.find_by(id: session[:user_id])
-        end
     end
 end
